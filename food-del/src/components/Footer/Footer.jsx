0760ec85fd9ad0f5173419288b7e3641efc59a7e
amit-simplify3x <amit.k@simplify3x.com>
2024-06-11 14:13:22 +0530

    Responsive

--- food-del/src/components/Footer/Footer.jsx
+++ food-del/src/components/Footer/Footer.jsx
@@ -7,34 +7,40 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis suscipit voluptatibus enim consequuntur iusto quae accusamus ad impedit sequi reprehenderit sunt, quo modi nobis facilis minus non nemo molestiae ut!</p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            suscipit voluptatibus enim consequuntur iusto quae accusamus ad
+            impedit sequi reprehenderit sunt, quo modi nobis facilis minus non
+            nemo molestiae ut!
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
         <div className="footer-content-center">
-            <h2>Company</h2>
-           <ul>
+          <h2>Company</h2>
+          <ul>
             <li>Home</li>
             <li>About us</li>
             <li>Delivery</li>
             <li>Privacy Polciy</li>
-           </ul>
+          </ul>
         </div>
 
         <div className="footer-content-right">
-              <h2>Get in touch</h2>
-              <ul>
-                <li>+91 234-456-0983</li>
-                <li>toamata@gmail.com</li>
-              </ul>
+          <h2>Get in touch</h2>
+          <ul>
+            <li>+91 234-456-0983</li>
+            <li>toamata@gmail.com</li>
+          </ul>
         </div>
       </div>
-    <p className="footer-copyright">Copyright 2024 @ tomatao.com - All Right Reserved.</p>
-
+      <p className="footer-copyright">
+        Copyright 2024 © tomatao.com - All Right Reserved.
+      </p>
     </div>
   );
 }
