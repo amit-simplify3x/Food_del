bb47f7fa402020e821797e1037e784d8f4c760b5
amit-simplify3x <amit.k@simplify3x.com>
2024-06-11 16:03:32 +0530

    login and signup page complete

---
+++ food-del/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './LoginPopup.css';
+import { assets } from '../../assets/assets';
+
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setcurrState] = useState("Login");
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} />
+        </div>
+        <div className="login-popup-inputs">
+          {currState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="enter your name" required />
+          )}
+          <input type="email" placeholder="enter your email" required />
+          <input type="password" placeholder="enter your password" required />
+        </div>
+        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>
+            By continuning, i agree to the terms of use and & privacy policy
+          </p>
+        </div>
+        {currState === "Login" ? (
+          <p>
+            Create a new account <span onClick={()=>setcurrState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account <span onClick={()=>setcurrState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup
--- food-del/src/components/Navbar/Navbar.jsx
+++ food-del/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,53 @@
 import React, { useState } from 'react';
 import './Navbar.css'
+import {Link} from 'react-router-dom'
 import {assets} from '../../assets/assets'
 
-const Navbar = () => {
-  const[menu,setmenu]=useState("home");
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setmenu] = useState("home");
   return (
-    <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo' />
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=>setmenu("home")} className={menu==="home"?"active":""}>home</li>
-        <li onClick={()=>setmenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-        <li onClick={()=>setmenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=>setmenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+        <Link
+          to="/"
+          onClick={() => setmenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setmenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setmenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setmenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
-          <div className="dot">
-
-          </div>
+          <div className="dot"></div>
         </div>
-        <button>Sign in</button>
+        <button onClick={()=>setShowLogin(true)}>Sign in</button>
       </div>
-       
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
