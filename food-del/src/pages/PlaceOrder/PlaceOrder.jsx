77d901a7114b5f12209ba46d6beb8be67ee04a8c
amit-simplify3x <amit.k@simplify3x.com>
2024-06-12 13:08:51 +0530

    create order page completed

--- food-del/src/pages/PlaceOrder/PlaceOrder.jsx
+++ food-del/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext';
 
 const PlaceOrder = () => {
+  const {getTotalCartAmount} = useContext(StoreContext)
   return (
-    <div>
-      
-    </div>
-  )
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email address" />
+        <input type="text" placeholder="Stree" />
+        <div className="multi-feilds">
+          <input type="text" placeholder="zip code" />
+          <input type="text" placeholder="country" />
+        </div>
+        <input type="text" placeholder="phone no" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>₹{getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>₹{2}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Total</p>
+            <p>₹{getTotalCartAmount() + 2}</p>
+          </div>
+          <button >
+            PROCEED TO PAYEMENT
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default PlaceOrder
