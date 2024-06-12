531162a712a7a288cc78ef71ddb55a907b277d8d
amit-simplify3x <amit.k@simplify3x.com>
2024-06-12 12:20:51 +0530

    cart page created

--- food-del/src/components/Navbar/Navbar.jsx
+++ food-del/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css'
 import {Link} from 'react-router-dom'
 import {assets} from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setmenu] = useState("home");
+
+  const {getTotalCartAmount}=useContext(StoreContext)
+
+
+
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/">
+       
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -41,10 +50,12 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>Sign in</button>
+        <button onClick={() => setShowLogin(true)}>Sign in</button>
       </div>
     </div>
   );
